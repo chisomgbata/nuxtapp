9df8ad135119c262e148e1798f0cf6f454911184
Chisom Chigbata <chisomgbata@gmail.com>
2023-04-04 08:02:58 +0100

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Count: {{ count }}</h1>
+    <button @click="count++">Increment</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+let count = ref(0);
+</script>
